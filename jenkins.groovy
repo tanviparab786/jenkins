7d9e0f5c4cce2c21b2023f00080264a65f5e6f5a
tanviparab <tanviparab786@gmail.com>
2025-07-02 15:56:59 +0530

    added stage deploying the code

--- jenkins.groovy
+++ jenkins.groovy
@@ -8,7 +8,7 @@ pipeline{
         }
         stage('deploying the code'){
             steps{
-                sh ' mv /var/lib/jenkins/workspace/frontend-pipeline/ /var/www/html/'
+                sh ' mv /var/lib/jenkins/workspace/frontend-pipeline/* /var/www/html/'
             }
         }
     }
