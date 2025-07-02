aca0f5682358d88637d922e99a7147e46faa225b
tanviparab <tanviparab786@gmail.com>
2025-07-02 15:25:09 +0530

    added stage deploying the code

--- jenkins.groovy
+++ jenkins.groovy
@@ -6,5 +6,10 @@ pipeline{
                 git branch: 'main', url: 'https://github.com/tanviparab786/cloth-store.git'
             }
         }
+        stage('deploying the code'){
+            steps{
+                sh 'mv /var/lib/jenkins/workspace/frontend-pipeline/ /var/www/html/'
+            }
+        }
     }
 }
